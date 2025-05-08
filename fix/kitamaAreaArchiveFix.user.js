7cf20c44325fb15fea873a904f33c75c7e20352c
skrige6666 <4022761+skrige6666@users.noreply.github.com>
2025-05-08 09:34:47 +0900

    KAAF

--- fix/kitamaAreaArchiveFix.user.js
+++ fix/kitamaAreaArchiveFix.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         kitamaAreaArchiveFix
+// @name         KitamaAreaArchiveFix
 // @namespace    https://kurone.co/
 // @version      v0
 // @description  修正されるまでの代用品
@@ -9,12 +9,14 @@
 // @grant        none
 // ==/UserScript==
 
+/* めちゃくちゃ雑な対応をしています */
+
 (($)=> {
-    $('a[href="area.php?list=1&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
-    $('a[href="area.php?list=2&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
+    $('a[href="area.php?list=1&p_id=0&archived=1#chat"]').attr('href','area.php?list=1&archived=1#chat');
+    $('a[href="area.php?list=2&p_id=0&archived=1#chat"]').attr('href','area.php?list=2&archived=1#chat');
     $('a[href="area.php?list=3&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
-    $('a[href="area.php?list=4&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
-    $('a[href="area.php?list=5&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
-    $('a[href="area.php?list=6&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
-    $('a[href="area.php?list=7&p_id=0&archived=1#chat"]').attr('href','area.php?list=3&archived=1#chat');
+    $('a[href="area.php?list=4&p_id=0&archived=1#chat"]').attr('href','area.php?list=4&archived=1#chat');
+    $('a[href="area.php?list=5&p_id=0&archived=1#chat"]').attr('href','area.php?list=5&archived=1#chat');
+    $('a[href="area.php?list=6&p_id=0&archived=1#chat"]').attr('href','area.php?list=6&archived=1#chat');
+    $('a[href="area.php?list=7&p_id=0&archived=1#chat"]').attr('href','area.php?list=7&archived=1#chat');
 })(jQuery);
