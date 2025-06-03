8ca1a80b0ab5b44179c3735d65038296909ad7c6
skrige6666 <4022761+skrige6666@users.noreply.github.com>
2025-06-04 02:40:02 +0900

    KNP

---
+++ util/kitamaNotificationPlus.user.js
@@ -0,0 +1,139 @@
+// ==UserScript==
+// @name         kitamaNotificationPlus
+// @namespace    https://kurone.co/
+// @version      0.1-beta
+// @description  未読が有ったら通知が出ます。
+// @author       skrige
+// @match        https://wdrb.work/otherside/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+// ***このスクリプトは未読チェックのために5分おきに自動で通信を行います。***
+
+// ***このスクリプトは未読チェックのために5分おきに自動で通信を行います。***
+
+// ***このスクリプトは未読チェックのために5分おきに自動で通信を行います。***
+
+// 実験的実装があります。
+// 何か問題が発生した場合は、スクリプトを無効化してください。
+// 導入は自己責任であり、このスクリプトを利用したことで何か問題が発生しても、一切の責任を負えません。
+
+//　機能
+// 未読のINFOやSURFがある場合に通知を出します。
+// 5分おきにINFOやSURFの未読フラグをチェックします。(home画面を利用します。画像等の取得は有りませんが通信が発生します。)
+// そのブラウザでの最後アクセス以降に「言及」「メッセージ」を含むACTIVITY更新があった場合に通知を出します。
+// (※アクティビティ処理はアクセスタイミングやタブ切り替えのタイミングで多少前後する場合があります)
+
+
+(($) => {
+  let currentTime = new Date().getTime();
+
+  //ページ読み込み時の処理
+  // 現在のページがinfo.phpの場合はスキップ
+  if (window.location.href.indexOf("info.php") === -1) {
+    $('a.new[href="info.php"]').length > 0
+      ? notification("未読のINFOがあります。")
+      : null;
+  }
+  // 現在のページがsurf.phpの場合はスキップ
+  if (window.location.href.indexOf("surf.php") === -1) {
+    $('a.new[href="surf.php"]').length > 0
+      ? notification("未読のSURFがあります。")
+      : null;
+  }
+
+  // 最終アクティブ時間以降のアクティビティチェック
+  const lastActiveTime = Number(GM_getValue("lastActiveTime", null));
+  if (lastActiveTime) {
+    const $activities = $("ul.activity li");
+    $activities.each(function () {
+      // 「2025-06-04 00:13:10」形式の日時文字列をDateオブジェクトに変換
+      const activityTime = new Date(
+        $(this)
+          .find("small.gray")
+          .text()
+          .replace(
+            /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/,
+            "$1/$2/$3 $4:$5:$6"
+          )
+      ).getTime();
+      if (activityTime > lastActiveTime) {
+        // pのテキスト内に「言及」「メッセージ」が含まれている場合のみ通知
+        const activityText = $(this).find("p").text().trim();
+        if (
+          activityText.includes("言及") ||
+          activityText.includes("メッセージ")
+        ) {
+          notification("新しいアクティビティ", $(this).find("p").text().trim());
+        }
+      }
+    });
+  }
+  GM_setValue("lastActiveTime", currentTime);
+
+  let checkInterval = null;
+  let lastCheckTime = currentTime;
+
+  $(document).on("visibilitychange", function () {
+    currentTime = new Date().getTime();
+    if (document.visibilityState === "hidden") {
+        //ブラウザのタブが非表示になった時の処理
+      //取得インターバルをクリア
+      clearInterval(checkInterval);
+
+      // 最終チェックタイムから5分経過している場合は最終操作時間を更新
+      if (lastCheckTime && currentTime - lastCheckTime >= 5 * 60 * 1000) {
+        GM_setValue("lastActiveTime", currentTime);
+      }
+    } else {
+      //取得インターバルを設定
+      checkInterval = setInterval(checkUnread, 5 * 60 * 1000);
+      // 最終チェックタイムから5分経過している場合は処理と最終操作時間を更新
+      if (lastCheckTime && currentTime - lastCheckTime >= 5 * 60 * 1000) {
+        checkUnread();
+        GM_setValue("lastActiveTime", currentTime);
+      }
+    }
+  });
+  async function checkUnread() {
+    //lastchecktimeを更新
+    lastCheckTime = new Date().getTime();
+    $.ajax({
+      url: "https://wdrb.work/otherside/home.php",
+      type: "GET",
+      success: function (data) {
+        // 受信したHTMLをjQueryオブジェクトに変換
+        const $html = $(data);
+
+        // INFO、SURFの未読チェック
+        if ($html.find('a.new[href="info.php"]').length > 0) {
+          notification("未読のINFOがあります。");
+          $('a[href="info.php"]').addClass("new");
+        } else {
+          $('a[href="info.php"]').removeClass("new");
+        }
+        if ($html.find('a.new[href="surf.php"]').length > 0) {
+          notification("未読のSURFがあります。");
+          $('a[href="surf.php"]').addClass("new");
+        } else {
+          $('a[href="surf.php"]').removeClass("new");
+        }
+      },
+      error: function (xhr, status, error) {
+        // エラーハンドリング
+        console.error(error);
+      },
+    });
+  }
+
+  function notification(title, text = null) {
+    $("div.noticearea").append(
+      $(
+        `<div class="notice"><p><span class="red">${title}</span>${
+          text ? "<br>" + text : ""
+        }</p></div>`
+      )
+    );
+  }
+})(jQuery);
